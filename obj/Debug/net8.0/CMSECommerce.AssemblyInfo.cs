7b47dd3b8673aa1ddd40ae8c7bb7ae559c40b2d4
Puttinan-171 <puttinan.c@gmail.com>
2024-12-11 09:31:28 +0700

    upload

--- obj/Debug/net8.0/CMSECommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/CMSECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMSECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f4f1a66838cbb1fa9d218f92337566975922a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMSECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMSECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
