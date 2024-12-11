e1f4f1a66838cbb1fa9d218f92337566975922a4
Puttinan-171 <puttinan.c@gmail.com>
2024-12-11 09:15:00 +0700

    v1

---
+++ Infrastructure/Component/MenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMSECommerce.Infrastructure.Component
+{
+    public class MenuViewComponent(Datacontext context):ViewComponent
+    {
+        private readonly Datacontext _context = context;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+              return View(await _context.Pages.ToListAsync());
+            
+        } 
+    }
+}
---
+++ Infrastructure/Datacontext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CMSECommerce.Infrastructure
+{
+    public class Datacontext(DbContextOptions<Datacontext>options):DbContext(options)
+    {
+        public DbSet<Page>Pages  { get; set; }//type prop
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder .Entity <Page>().HasData(
+                new Page { Id = 1, Title="Home",Slug="home",Body="This is the home page" },
+                new Page { Id = 2, Title="About",Slug="about",Body="This is the about page" },
+                new Page { Id = 3, Title="Services",Slug="services",Body="This is the service page" },
+                new Page { Id = 4, Title="Contact",Slug="contact",Body="This is the contact page" }
+            ) ;
+        }
+    }
+}
